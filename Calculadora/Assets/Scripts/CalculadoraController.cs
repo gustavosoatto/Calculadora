115976727484a5cc0f309182819f2b92f5d4e507
GustavoSoatto <gustavo.soatto@hotmail.com>
2022-10-28 01:31:14 -0300

    MyCalculator
    Criação de uma calculadora simples com diversos temas utilizando Unity

--- Calculadora/Assets/Scripts/CalculadoraController.cs
+++ Calculadora/Assets/Scripts/CalculadoraController.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Data;
 
 public class CalculadoraController : MonoBehaviour
 {
 
-    [SerializeField]    private Text TextDisplayI;
-    [SerializeField]    private Text TextDisplayII;
+    [SerializeField] private Text TextDisplayI;
+    [SerializeField] private Text TextDisplayII;
+
+    private string _expression;
+    private string _clear;
+    private string _result;
+    
     void Start()
     {
         TextDisplayI.text = "";
@@ -18,6 +24,31 @@ public class CalculadoraController : MonoBehaviour
     {
         TextDisplayII.text = character;
         TextDisplayI.text = TextDisplayI.text + TextDisplayII.text;
+    }
+
+    public void ExpressionCalculator()
+    {
+        _expression = TextDisplayI.text;
+        DataTable table = new DataTable("tabela");
+        _result = table.Compute(_expression, string.Empty).ToString();
+        TextDisplayII.text = _result;
+        TextDisplayI.text = "";
+    }
+
+    public void ClearAll()
+    {
+        TextDisplayI.text = "";
+        TextDisplayII.text = "";
+    }
+
+    public void Clear()
+    {
+        _clear = TextDisplayI.text;
+        string[] displayI = _clear.Split("");
+        int tamanhoI = displayI.Length;
+        int tamanhoF = tamanhoI - 1;
+        displayI[tamanhoF] = "";
+        TextDisplayI.text = string.Join(" ", displayI);
     }
 
 }
---
+++ Calculadora/Assets/Scripts/Themes.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+//This example outputs Sliders that control the red green and blue elements of a sprite's color
+//Attach this to a GameObject and attach a SpriteRenderer component
+
+public class Themes : MonoBehaviour
+{
+//    [SerializeField] private Color cyanBackground = Color.cyan;
+    [SerializeField] private Color blackBackground;
+    [SerializeField] private Color redBackground;
+    [SerializeField] private Color yellowBackground;
+    [SerializeField] private Color greenBackground;
+    [SerializeField] private Color cyanBackground;
+    [SerializeField] private Color blueBackground;
+    [SerializeField] private Color magentaBackground;
+    [SerializeField] private Color whiteBackground;
+    
+
+    //ui component
+    [SerializeField] private SpriteRenderer m_SpriteRenderer;
+
+    //private int contador = 0;
+    //float m_Red, m_Blue, m_Green;
+
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        blackBackground = new Color(0.2f,0.2f, 0.2f,0.5f);
+        redBackground = new Color(0.8f, 0.0f, 0.0f, 0.5f);
+        yellowBackground = new Color(0.8f, 0.8f, 0.0f, 0.5f);
+        greenBackground = new Color(0.0f, 0.8f, 0.0f, 0.5f);
+        cyanBackground = new Color(0.0f, 0.8f, 0.8f, 0.5f);
+        blueBackground = new Color(0.0f, 0.0f, 0.8f, 0.5f);
+        magentaBackground = new Color(0.8f, 0.0f, 0.8f, 0.5f);
+        whiteBackground = new Color(0.8f, 0.8f, 0.8f, 0.5f);
+
+        m_SpriteRenderer.color = blackBackground;
+    }
+
+    public void ChoiceTheme()
+    {
+        if (m_SpriteRenderer.color == blackBackground)
+        {
+            m_SpriteRenderer.color = redBackground;
+        }
+        else if (m_SpriteRenderer.color == redBackground)
+        {
+            m_SpriteRenderer.color = yellowBackground;
+        }
+        else if (m_SpriteRenderer.color == yellowBackground)
+        {
+            m_SpriteRenderer.color = greenBackground;
+        }
+        else if (m_SpriteRenderer.color == greenBackground)
+        {
+            m_SpriteRenderer.color = cyanBackground;
+        }
+        else if (m_SpriteRenderer.color == cyanBackground)
+        {
+            m_SpriteRenderer.color = blueBackground;
+        }
+        else if (m_SpriteRenderer.color == blueBackground)
+        {
+            m_SpriteRenderer.color = magentaBackground;
+        }
+        else if (m_SpriteRenderer.color == magentaBackground)
+        {
+            m_SpriteRenderer.color = whiteBackground;
+        }
+        else{
+            m_SpriteRenderer.color = blackBackground;
+        }
+    }
+}
